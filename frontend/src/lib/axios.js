1093e47e7bc2a944df08999896153d20ad0763b4
021jadeja <rudrarajsinhjadeja160@gmail.com>
2025-04-18 10:29:44 +0530

    undo axios and .env(news)

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,19 +1,8 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-	baseURL:
-		import.meta.env.MODE === "development"
-			? "http://localhost:5000/api/v1"
-			: "https://backend-6dny.onrender.com/api/v1",
+export const axiosInstance = axios.create({
+	baseURL: import.meta.env.MODE === "development" 
+		? "http://localhost:5000/api/v1" 
+		: "https://backend-6dny.onrender.com/api/v1",
+	withCredentials: true,
 });
-
-// Add a request interceptor to set the token
-axiosInstance.interceptors.request.use((config) => {
-	const token = localStorage.getItem("token");
-	if (token) {
-		config.headers.Authorization = `Bearer ${token}`;
-	}
-	return config;
-});
-
-export { axiosInstance };
